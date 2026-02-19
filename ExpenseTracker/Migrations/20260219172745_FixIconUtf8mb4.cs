16805f504d6b4c4937a871310a48450f0223170b
saksham.kc <saksham.kc@cotiviti.com>
2026-02-19 17:35:09 +0000

    icon fixed

---
+++ ExpenseTracker/Migrations/20260219172745_FixIconUtf8mb4.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixIconUtf8mb4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE Categories CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+            migrationBuilder.Sql("ALTER TABLE Transactions CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+            migrationBuilder.Sql("ALTER TABLE CalendarEvents CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
